f65a6409c21cf85ea85e6f423122aa38e9c54b16
Aaron3312 <a01642529@tec.mx>
2024-08-07 12:24:36 -0600

    cambios limite tareas

--- ProyectoCronos/nodejs-mvc-boilerplate-master/src/server.js
+++ ProyectoCronos/nodejs-mvc-boilerplate-master/src/server.js
@@ -257,6 +257,9 @@ app.post("/databases", async function (req, res) {
 	var promptNumberOfTasks =
 		"Genera y dime cuantas tareas tiene el proyecto unicamente genera el numero de tareas nada mas, generalo en formato numerico sin otra palabra!";
 	var NumberOfTasks = await DBsd(readMessages(), promptNumberOfTasks); //numero de tareas del proyecto
+	if (NumberOfTasks > 20) {
+		NumberOfTasks = 20;
+	} //limitamos el numero de tareas a 20
 	projectDetails.NumberOfTasks.push(NumberOfTasks);
 
 	//generamos la primera tarea del proyecto ########################################
