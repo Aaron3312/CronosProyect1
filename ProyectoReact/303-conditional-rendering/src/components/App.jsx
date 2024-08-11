cb0d05d00294cf1fbd8abd6103855ba5a398de82
Aaron3312 <a01642529@tec.mx>
2024-08-11 03:49:49 -0600

    finalizando 304

--- ProyectoReact/303-conditional-rendering/src/components/App.jsx
+++ ProyectoReact/303-conditional-rendering/src/components/App.jsx
@@ -1,16 +1,24 @@
 import React from "react";
+import Login from "./Login";
+
+var isloggedIn = true;
+
+function renderConditionally(){
+  if (isloggedIn === true){
+    return <h1>Hello</h1>
+  } else {
+    return ( <Login /> )
+  }
+}
+
+const currentTime = new Date().getSeconds();
+console.log(currentTime);
+
+
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+//  return <div className="container"> {isloggedIn ? <h1>Hello!</h1> : <Login />} </div>
+  return <div className="container"> {(currentTime%2) === 0 && currentTime > 30 ? <h1>why are u still working?</h1> : <h1>why are u still sleeping?</h1>} </div>
 }
 
 export default App;
