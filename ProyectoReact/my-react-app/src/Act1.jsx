e8587840fa1d80c41ddfda05034785ec4b86e090
Aaron3312 <a01642529@tec.mx>
2024-08-10 18:18:20 -0600

    iniciando R act

---
+++ ProyectoReact/my-react-app/src/Act1.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './Act1.css'
+
+function Act1() {
+    return (
+        <div className="Act1">
+            <h1>Actividad 1</h1>
+            <ul>
+                <li>elemento1</li>
+                <li>elemento2</li>
+                <li>elemento3</li>
+                <li>elemento4</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Act1
--- ProyectoReact/my-react-app/src/main.jsx
+++ ProyectoReact/my-react-app/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Act1 from './Act1.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Act1 />
     <App />
   </StrictMode>,
 )
