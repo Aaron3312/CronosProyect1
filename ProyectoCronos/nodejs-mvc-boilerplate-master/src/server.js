f42258553694dd746af84644aa149e834adbb32c
Aaron3312 <a01642529@tec.mx>
2024-08-11 00:26:55 -0600

    deadly?

--- ProyectoCronos/nodejs-mvc-boilerplate-master/src/server.js
+++ ProyectoCronos/nodejs-mvc-boilerplate-master/src/server.js
@@ -470,8 +470,8 @@ async function pageGenerator(response2, pageId, response_T1, dueDate) {
 		var response_T1_2 = response_T1.slice(2000, response_T1.length);
 		var response2_1 = response2 + " Parte 2";
 		//mandamos llamar la funcion para generar la pagina de la tarea en dos partes
-		var page1 = await pageGenerator1(response2, pageId, response_T1_1, dueDate);
-		//var page2 = await pageGenerator1(response2_1, pageId, response_T1_2, dueDate);
+		var page1 = await pageGenerator(response2, pageId, response_T1_1, dueDate);
+		var page2 = await pageGenerator(response2_1, pageId, response_T1_2, dueDate);
 	} else {
 	try {
 		const newPage = await notion.pages.create({
