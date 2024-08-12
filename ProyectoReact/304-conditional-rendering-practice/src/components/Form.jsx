2f97d20323c5caa18ef3def5721d90c98fa6230c
Aaron3312 <a01642529@tec.mx>
2024-08-11 21:29:12 -0600

    End act 4

--- ProyectoReact/304-conditional-rendering-practice/src/components/Form.jsx
+++ ProyectoReact/304-conditional-rendering-practice/src/components/Form.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 
-function Form() {
+function buttonName(userIsRegistered) {
+  return userIsRegistered ? "Login" : "Register";
+}
+
+function confirmPassword(userIsRegistered) {
+  return userIsRegistered ? null : <input type="password" placeholder="Confirm Password" />;
+}
+
+function confirmPasswordLarge(userIsRegistered) {
+  if (userIsRegistered) {
+    return null;
+  } else {
+    return <input type="password" placeholder="Confirm Password" />;
+  }
+}
+
+
+function Form(props) {
   return (
     <form className="form">
       <input type="text" placeholder="Username" />
       <input type="password" placeholder="Password" />
-      <input type="password" placeholder="Confirm Password" />
-      <button type="submit">Register</button>
+      {confirmPasswordLarge(props.userIsRegistered)}
+      <button type="submit">{buttonName(props.userIsRegistered)}</button>
     </form>
   );
 }
